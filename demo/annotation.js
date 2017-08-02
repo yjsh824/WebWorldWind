4b54d85a35f8b707f745d0c8dfd15454b06de4f9
yjsh824 <yjsh824@163.com>
2017-08-02 09:13:27 +0800

    实现舜泰广场的三维建筑并贴了纹理

---
+++ demo/annotation.js
@@ -0,0 +1,53 @@
+/**
+ * Created by yuanjs on 2017/7/31.
+ */
+requirejs([
+    '../src/WorldWind'
+],function(ww){
+    "use strict";
+
+    WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
+
+    var wwd = new WorldWind.WorldWindow("canvasOne");
+
+    var layers = [
+        {layer: new WorldWind.TiandituLayer(), enabled: true},
+        {layer: new WorldWind.CompassLayer(), enabled: true},
+        {layer: new WorldWind.CoordinatesDisplayLayer(wwd), enabled: true},
+        {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true}
+    ];
+
+    for (var l = 0; l < layers.length; l++) {
+        layers[l].layer.enabled = layers[l].enabled;
+        wwd.addLayer(layers[l].layer);
+    }
+    var annotationAttributes = new WorldWind.AnnotationAttributes(null);
+    annotationAttributes.cornerRadius = 14;//设置指示框角弧度
+    annotationAttributes.backgroundColor = WorldWind.Color.BLUE;//设置背景色
+    annotationAttributes.drawLeader = true;//是否显示指示框
+    annotationAttributes.leaderGapWidth = 40;//指示框的指示尖宽度
+    annotationAttributes.leaderGapHeight = 30;//指示框的指示尖高度
+    annotationAttributes.opacity = 1;//设置不透明度
+    annotationAttributes.scale = 1;//设置比例尺
+    annotationAttributes.width = 200;//设置宽度
+    annotationAttributes.height = 100;//设置高度
+    annotationAttributes.textAttributes.color = WorldWind.Color.WHITE;
+    annotationAttributes.insets = new WorldWind.Insets(10, 10, 10, 10);//指示框内部的padding
+
+    // Set a location for the annotation to point to and create it.
+    var location = new WorldWind.Position(39.899,116.409, 1e2);
+    var annotation = new WorldWind.Annotation(location, annotationAttributes);
+    // Text can be assigned to the annotation after creating it.
+    annotation.label = "消息指示框。\n大家好";
+
+    // Create and add the annotation layer to the World Window's layer list
+    var annotationsLayer = new WorldWind.RenderableLayer("Annotations");
+    annotationsLayer.addRenderable(annotation);
+    wwd.addLayer(annotationsLayer);
+
+    var flyto=new WorldWind.Position();
+    flyto=flyto.copy(location);
+    flyto.altitude =1e6;
+    wwd.goTo(flyto);
+
+});
---
+++ demo/shapefile.js
@@ -0,0 +1,139 @@
+/**
+ * Created by yuanjs on 2017/7/31.
+ */
+requirejs([
+    '../src/WorldWind'
+],function(ww){
+    "use strict";
+
+    WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
+
+    var wwd = new WorldWind.WorldWindow("canvasOne");
+
+    var layers = [
+        //{layer: new WorldWind.BMNGLayer(), enabled: true},
+       {layer: new WorldWind.TiandituLayer(), enabled: true},
+        {layer: new WorldWind.CompassLayer(), enabled: true},
+        {layer: new WorldWind.CoordinatesDisplayLayer(wwd), enabled: true},
+        {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true}
+    ];
+
+    for (var l = 0; l < layers.length; l++) {
+        layers[l].layer.enabled = layers[l].enabled;
+        wwd.addLayer(layers[l].layer);
+    }
+
+
+    // Set up the common placemark attributes.
+    var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
+    placemarkAttributes.imageScale = 0.025;
+    placemarkAttributes.imageColor = WorldWind.Color.WHITE;
+    placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
+        WorldWind.OFFSET_FRACTION, 0.5,
+        WorldWind.OFFSET_FRACTION, 1.0);
+    placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "images/white-dot.png";
+
+
+    var drawpolygon=function(pts){
+        var boundaries = [];
+        boundaries[0] = []; // outer boundary
+
+
+        for(var i=0;i<pts.length;i=i+2)
+        {
+            boundaries[0].push(new WorldWind.Position(pts[i+1],pts[i],100));
+        }
+
+
+        var polygon = new WorldWind.Polygon(boundaries, null);
+        polygon.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
+        polygon.extrude = true;
+        polygon.textureCoordinates = [
+            [new WorldWind.Vec2(0, 0), new WorldWind.Vec2(1, 0), new WorldWind.Vec2(1, 1), new WorldWind.Vec2(0, 1)]
+        ];
+
+        var polygonAttributes = new WorldWind.ShapeAttributes(null);
+        // Specify a texture for the polygon and its four extruded sides.
+        polygonAttributes.imageSource = [
+            "../images/texture1.jpg", // polygon texture image
+            "../images/texture1.jpg", // first-side texture image
+            "../images/texture1.jpg", // second-side texture image
+            "../images/texture1.jpg", // third-side texture image
+            "../images/texture1.jpg"  // fourth-side texture image
+        ];
+        polygonAttributes.drawInterior = true;
+        polygonAttributes.drawOutline = false;
+        polygonAttributes.outlineColor = WorldWind.Color.WHITE;
+        polygonAttributes.interiorColor = WorldWind.Color.WHITE;
+        polygonAttributes.drawVerticals = polygon.extrude;
+        polygonAttributes.applyLighting = true;
+        polygonAttributes.depthTest=false;
+        polygon.attributes = polygonAttributes;
+        // var highlightAttributes = new WorldWind.ShapeAttributes(polygonAttributes);
+        // highlightAttributes.outlineColor = WorldWind.Color.WHITE;
+        // polygon.highlightAttributes = highlightAttributes;
+
+        polygonsLayer.addRenderable(polygon);
+    };
+    var polygonsLayer = new WorldWind.RenderableLayer();
+    polygonsLayer.displayName = "Polygons";
+    wwd.addLayer(polygonsLayer);
+    var shapeConfigurationCallback = function (attributes, record) {
+        // console.log(attributes);
+        // console.log(record);
+        drawpolygon(record._parts[0]);
+        var configuration = {};
+        configuration.name = attributes.values.name || attributes.values.Name || attributes.values.NAME;
+
+        if (record.isPointType()) {
+            configuration.name = attributes.values.name || attributes.values.Name || attributes.values.NAME;
+
+            configuration.attributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
+
+            if (attributes.values.pop_max) {
+                var population = attributes.values.pop_max;
+                configuration.attributes.imageScale = 0.01 * Math.log(population);
+            }
+        } else if (record.isPolygonType()) {
+            configuration.attributes = new WorldWind.ShapeAttributes(null);
+
+            // Fill the polygon with a random pastel color.
+            configuration.attributes.interiorColor = new WorldWind.Color(
+                0.375 + 0.5 * Math.random(),
+                0.375 + 0.5 * Math.random(),
+                0.375 + 0.5 * Math.random(),
+                1.0);
+
+            // Paint the outline in a darker variant of the interior color.
+            configuration.attributes.outlineColor = new WorldWind.Color(
+                0.5 * configuration.attributes.interiorColor.red,
+                0.5 * configuration.attributes.interiorColor.green,
+                0.5 * configuration.attributes.interiorColor.blue,
+                1.0);
+        }
+
+        return configuration;
+    };
+
+   // var shapefileLibrary = "http://worldwindserver.net/webworldwind/data/shapefiles/naturalearth";
+
+    // Create data for the world.
+    var worldLayer = new WorldWind.RenderableLayer("house1");
+    var worldShapefile = new WorldWind.Shapefile( "./shapefile/house1.shp");
+    worldShapefile.load(null, shapeConfigurationCallback, worldLayer);
+    wwd.addLayer(worldLayer);
+    wwd.navigator.tilt=75;
+    wwd.goTo(new   WorldWind.Position(36.665,117.136, 2e3));
+    //
+    // // Create data for cities.
+    // var cityLayer = new WorldWind.RenderableLayer("Cities");
+    // var cityShapefile = new WorldWind.Shapefile(shapefileLibrary + "/ne_50m_populated_places_simple/ne_50m_populated_places_simple.shp");
+    // cityShapefile.load(null, shapeConfigurationCallback, cityLayer);
+    // wwd.addLayer(cityLayer);
+    //
+    // var fortStory = "http://worldwindserver.net/webworldwind/data/shapefiles/misc/FortStory/Trident-Spectre-Indigo-i.shp";
+    // var fortStoryLayer = new WorldWind.RenderableLayer("Fort Story");
+    // var fortStoryShapefile = new WorldWind.Shapefile(fortStory);
+    // fortStoryShapefile.load(null, null, fortStoryLayer);
+    // wwd.addLayer(fortStoryLayer);
+});
